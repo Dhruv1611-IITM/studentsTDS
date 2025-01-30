4169ddd5987de01f7404156e8d09331af8de4bf7
Dhruv Banerjee <dhruvbanerjee6263@gmail.com>
2025-01-30 13:14:50 +0530

    2nd err

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
 )
 
 # Load student marks from JSON file
-MARKS_FILE = Path("students\data\q-vercel-python.json")
+MARKS_FILE = Path("./data/q-vercel-python.json")
 students = {}
 
 try:
